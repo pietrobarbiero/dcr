f54202d48b91adddf918f13e2f3c64c209c81a7f
pietro <barbiero@tutanota.com>
2023-01-06 11:27:45 +0000

    Add non-linear model to compute attention scores (more stable) 🌟

--- dcr/nn.py
+++ dcr/nn.py
@@ -1,14 +1,15 @@
-import math
 from collections import Counter
 import torch
 
 from .semantics import Logic
 
 EPS = 1e-3
+MAX_EXP = 30
 
 
 def softmaxnorm(values, temperature):
-    softmax_scores = torch.exp(values / temperature) / torch.sum(torch.exp(values / temperature), dim=1, keepdim=True)
+    x = torch.clamp_max(values / temperature, MAX_EXP)
+    softmax_scores = torch.exp(x) / torch.sum(torch.exp(x), dim=1, keepdim=True)
     return softmax_scores / softmax_scores.max(dim=1)[0].unsqueeze(1)
 
 
@@ -18,22 +19,17 @@ class ConceptReasoningLayer(torch.nn.Module):
         self.emb_size = emb_size
         self.n_classes = n_classes
         self.logic = logic
+        self.filter_nn = torch.nn.Sequential(
+            torch.nn.Linear(emb_size, emb_size),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(emb_size, n_classes),
+        )
+        self.sign_nn = torch.nn.Sequential(
+            torch.nn.Linear(emb_size, emb_size),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(emb_size, n_classes),
+        )
         self.temperature_complexity = temperature_complexity
-        self.w_key_logic = torch.nn.Parameter(torch.empty((emb_size, emb_size)))
-        self.w_query_logic = torch.nn.Parameter(torch.empty((emb_size, n_classes)))
-        self.w_key_filter = torch.nn.Parameter(torch.empty((emb_size, emb_size)))
-        self.w_query_filter = torch.nn.Parameter(torch.empty((emb_size, n_classes)))
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
-        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
-        # https://github.com/pytorch/pytorch/issues/57109
-        # torch.nn.init.kaiming_uniform_(self.w_value, a=math.sqrt(5))
-        torch.nn.init.kaiming_uniform_(self.w_key_logic, a=math.sqrt(5))
-        torch.nn.init.kaiming_uniform_(self.w_query_logic, a=math.sqrt(5))
-        torch.nn.init.kaiming_uniform_(self.w_key_filter, a=math.sqrt(5))
-        torch.nn.init.kaiming_uniform_(self.w_query_filter, a=math.sqrt(5))
 
     def forward(self, x, c, return_attn=False, sign_attn=None, filter_attn=None):
         values = c.unsqueeze(-1).repeat(1, 1, self.n_classes)
@@ -42,21 +38,15 @@ class ConceptReasoningLayer(torch.nn.Module):
         if sign_attn is None:
             # compute attention scores to build logic sentence
             # each attention score will represent whether the concept should be active or not in the logic sentence
-            logic_keys = x @ self.w_key_logic   # TODO: might be independent of input x (but requires OR)
-            sign_attn = torch.sigmoid(logic_keys @ self.w_query_logic)
+            sign_attn = torch.sigmoid(self.sign_nn(x))  # TODO: might be independent of input x (but requires OR)
 
         # attention scores need to be aligned with predicted concept truth values (attn <-> values)
         # (not A or V) and (A or not V) <-> (A <-> V)   # TODO: Fra check
         sign_terms = self.logic.iff_pair(sign_attn, values)    # TODO: temperature sharp here?
-        # control sharpness of truth values
-        # sharper values -> lower leakage, lower accuracy
-        # less sharp values -> higher leakage, higher accuracy
-        # sign_terms = torch.sigmoid(self.temperature_sharp * (sign_terms - 0.5))
 
         if filter_attn is None:
             # compute attention scores to identify only relevant concepts for each class
-            filter_keys = x @ self.w_key_filter   # TODO: might be independent of input x (but requires OR)
-            filter_attn = softmaxnorm(filter_keys @ self.w_query_filter, self.temperature_complexity)
+           filter_attn = softmaxnorm(self.filter_nn(x), self.temperature_complexity)   # TODO: might be independent of input x (but requires OR)
 
         # filter values
         # filtered implemented as "or(a, not b)", corresponding to "b -> a"
@@ -69,7 +59,7 @@ class ConceptReasoningLayer(torch.nn.Module):
         # TODO: add OR for global explanations
 
         if return_attn:
-            return preds, sign_attn, filter_attn   # FIXME: handle None cases
+            return preds, sign_attn, filter_attn
         else:
             return preds
 

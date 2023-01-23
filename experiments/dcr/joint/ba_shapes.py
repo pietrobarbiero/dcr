5cb1d21a5e69541c480a126cb7d0a9ce254f0969
Charlotte Magister <charlottemagister@googlemail.com>
2023-01-23 12:54:48 +0000

    BA shapes changes

--- experiments/dcr/joint/ba_shapes.py
+++ experiments/dcr/joint/ba_shapes.py
@@ -1,59 +1,48 @@
-from torch.nn import BCELoss
-import pytorch_lightning as pl
-import torch
+# import sys
+# sys.path.append('..')
 import os
-from pytorch_lightning.loggers import TensorBoardLogger
-from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
-from dcr.data.ba_shapes import load_ba_shapes
-from dcr.data.celeba import load_celeba
-from dcr.semantics import Logic, GodelTNorm
-from dcr.nn import ConceptReasoningLayer, ConceptEmbedding
-
-from torch_geometric.nn import GCNConv
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, dense_diff_pool
 
-class DeepGraphConceptReasoner(pl.LightningModule):
-    def __init__(self, num_in_features, in_concepts, out_concepts, emb_size, concept_names, class_names,
-                 learning_rate, loss_form, concept_loss_weight: float = 1., class_loss_weight: float = 1.,
-                 temperature: float = 1., reasoner: bool = True,
-                 logic: Logic = GodelTNorm()):
-        super().__init__()
-        self.concept_loss_weight = concept_loss_weight
-        self.class_loss_weight = class_loss_weight
-        # self.resnet = torchvision.models.resnet18(pretrained=True)
-        # for param in self.resnet.parameters():
-        #     param.requires_grad = False
-        # n_features = self.resnet.fc.in_features
-        # self.resnet.fc = torch.nn.Sequential(
-        #     torch.nn.Linear(n_features, n_features),
-        #     torch.nn.LeakyReLU(),
-        # )
+import numpy as np
+import pandas as pd
+from pytorch_lightning.utilities.seed import seed_everything
+from scipy.spatial.distance import cdist
 
-        num_hidden_features = 20
+from dcr.data.ba_shapes import load_ba_shapes, NUMBER_OF_CLASSES, load_ba_shapes_node_class
+
+from sklearn.cluster import KMeans
+
+import ba_shapes_model_utils
+from torch_geometric.loader import DataLoader
+
+global activation_list
+activation_list = {}
+
+def register_hooks(model):
+    for name, m in model.named_modules():
+            if isinstance(m, GCNConv) or isinstance(m, nn.Linear) or isinstance(m, DenseGCNConv):
+                m.register_forward_hook(get_activation(f"{name}"))
+
+    return model
+
+from sklearn.metrics.cluster import completeness_score
+
+class GCN(nn.Module):
+    def __init__(self, num_in_features, num_hidden_features, num_classes):
+        super(GCN, self).__init__()
 
         self.conv0 = GCNConv(num_in_features, num_hidden_features)
         self.conv1 = GCNConv(num_hidden_features, num_hidden_features)
         self.conv2 = GCNConv(num_hidden_features, num_hidden_features)
         self.conv3 = GCNConv(num_hidden_features, num_hidden_features)
 
-        n_features = num_hidden_features
-
-        self.concept_embedder = ConceptEmbedding(n_features, in_concepts, emb_size)
-        self.reasoner = reasoner
-        if self.reasoner:
-            self.predictor = ConceptReasoningLayer(emb_size, out_concepts, logic, temperature)
-        else:
-            self.predictor = torch.nn.Sequential(
-                torch.nn.Linear(in_concepts, 10),
-                torch.nn.LeakyReLU(),
-                torch.nn.Linear(10, out_concepts),
-                torch.nn.Sigmoid()
-            )
-        self.concept_names = concept_names
-        self.class_names = class_names
-        self.learning_rate = learning_rate
-        self.loss_form = loss_form
+        # linear layers
+        self.linear = nn.Linear(num_hidden_features, num_classes)
 
     def forward(self, x, edge_index):
         x = self.conv0(x, edge_index)
@@ -68,131 +57,125 @@ class DeepGraphConceptReasoner(pl.LightningModule):
         x = self.conv3(x, edge_index)
         x = F.leaky_relu(x)
 
+        x = self.linear(x)
 
-        c_emb, c_pred = self.concept_embedder(x)
-
-        if self.reasoner:
-            y_pred = self.predictor(c_emb, c_pred)
-        else:
-            y_pred = self.predictor(c_pred)
-        return c_pred, y_pred
+        return F.log_softmax(x, dim=-1)
 
 
-def test(c, y, c_pred, y_pred, mask):
-    c_pred = torch.round(c_pred)
-    y = y.max(dim=1)[1]
-    y_pred = y_pred.max(dim=1)[1]
 
-    c_correct = torch.all(torch.eq(c[mask], c_pred[mask]), dim=1).sum().item()
-    y_correct = y_pred[mask].eq(y[mask]).sum().item()
+def run_experiment(seed, fold):
+    epochs = 2000
+    lr = 0.001
+    batch_size = 32
+    num_hidden_units = 10
+    num_classes = 7
+    K = 12
 
-    return c_correct / (len(c[mask])), y_correct / (len(y[mask]))
+    # load data
+    data = load_ba_shapes_node_class()
 
-
-def training_loop(model, data, epochs, lr):
-    # get data
     x = data["x"]
     edges = data['edges']
+    edges_t = data['edge_list'].numpy()
     y = data["y"]
-    c = data["concept_labels"]
     train_mask = data["train_mask"]
     test_mask = data["test_mask"]
-    optimizer = torch.optim.AdamW(model.parameters(), lr=model.learning_rate)
 
-    # list of accuracies
-    train_accuracies_c, train_accuracies_y, test_accuracies_c, test_accuracies_y, train_losses, test_losses = list(), list(), list(), list(), list(), list()
+    # model training
+    model = GCN(data["x"].shape[1], num_hidden_units, num_classes)
 
-    # iterate for number of epochs
-    for epoch in range(epochs):
-        # set mode to training
-        model.train()
+    # register hooks to track activation
+    model = ba_shapes_model_utils.register_hooks(model)
 
-        # input data
-        optimizer.zero_grad()
+    # train
+    train_acc, test_acc, train_loss, test_loss = ba_shapes_model_utils.train(model, data, epochs, lr, if_interpretable_model=False)
+    print("Testing Acc: ", test_acc[-1])
 
-        c_pred, y_pred = model(x, edges)
+    # get model activations for complete dataset
+    pred = model(x, edges)
 
+    activation = torch.squeeze(ba_shapes_model_utils.activation_list['conv3']).detach().numpy()
 
-        concept_loss = model.loss_form(c_pred[train_mask], c[train_mask].float())
-        class_loss = model.loss_form(y_pred[train_mask], y[train_mask].float())
-        loss = model.concept_loss_weight * concept_loss + model.class_loss_weight * class_loss
-        model.log("train_loss", loss.item())
-        model.log("concept_loss", concept_loss.item())
-        model.log("class_loss", class_loss.item())
-
-        loss.backward()
-        optimizer.step()
+    # find centroids
+    kmeans_model = KMeans(n_clusters=K, random_state=seed)
+    kmeans_model = kmeans_model.fit(activation[train_mask])
+    used_centroid_labels = kmeans_model.predict(activation)
+    centroid_labels = np.sort(np.unique(used_centroid_labels))
+    centroids = kmeans_model.cluster_centers_
 
 
-        model.eval()
-        c_pred, y_pred = model(x, edges)
+    # clustering efficency
+    completeness = completeness_score(y, used_centroid_labels)
+    print("Completeness: ", completeness)
 
-        concept_loss = model.loss_form(c_pred[test_mask], c[test_mask].float())
-        class_loss = model.loss_form(y_pred[test_mask], y[test_mask].float())
-        test_loss = model.concept_loss_weight * concept_loss + model.class_loss_weight * class_loss
-        model.log("test_loss", test_loss.item())
+    # # plot clustering
+    # ba_shapes_model_utils.plot_clustering(seed, activation, y, centroids, centroid_labels, used_centroid_labels)
+    # plt.close()
 
-        train_acc_c, train_acc_y = test(c, y, c_pred, y_pred, train_mask)
-        test_acc_c, test_acc_y = test(c, y, c_pred, y_pred, test_mask)
+    # save concept scores - 3 colours plus 4 structures
+    number_of_concepts = 7
+    concept_scores = []
+    for l in used_centroid_labels:
+        concept_vector = np.zeros(number_of_concepts)
+        res_sorted = kmeans_model.transform(activation)
+        distances = res_sorted[:, l]
+        node_idx = np.argsort(distances)[::][0]
+        colour = x[node_idx][0]
+        structure = y[node_idx]
 
-        ## add to list and print
-        train_accuracies_c.append(train_acc_c)
-        train_accuracies_y.append(train_acc_y)
-        test_accuracies_c.append(test_acc_c)
-        test_accuracies_y.append(test_acc_y)
-        train_losses.append(loss.item())
-        test_losses.append(test_loss.item())
+        concept_vector[structure] = 1
 
-        print('Epoch: {:03d}, Loss: {:.5f}, Train Acc C: {:.5f}, Train Acc Y: {:.5f}, Test Acc C: {:.5f}, Test Acc Y: {:.5f}'.
-              format(epoch, loss.item(), train_acc_c, train_acc_y, test_acc_c, test_acc_y), end = "\r")
+        if colour == 1:
+            concept_vector[4] = 1
+        elif colour == 24:
+            concept_vector[5] = 1
+        elif colour == 80:
+            concept_vector[6] = 1
 
-    model.eval()
-    return train_accuracies_c, train_accuracies_y, test_accuracies_c, test_accuracies_y
+        concept_scores.append(concept_vector)
+
+    # print(concept_scores)
+    concept_scores = np.array(concept_scores)
+    # print(concept_scores.shape)
+
+
+    # save embedding encoding
+    embedding_encoding = []
+    for concept_vector in concept_scores:
+        embedding_vector = []
+        for i, c in enumerate(concept_vector):
+            if c == 1:
+                embedding_vector.append(np.array(centroids[i]))
+            else:
+                embedding_vector.append(np.zeros(centroids[0].shape))
+
+        embedding_encoding.append(embedding_vector)
+
+    # print(embedding_encoding)
+    embedding_encoding = np.array(embedding_encoding)
+    # print(embedding_encoding.shape)
+
+    # train_mask
+    # test_mask
+    path = f'./results/ba_shapes/{fold}'
+    torch.save(model.state_dict(), f'{path}/model.pt')
+    np.save(f'{path}/embedding_encoding.npy', embedding_encoding)
+    np.save(f'{path}/concept_scores.npy', concept_scores)
+    np.save(f'{path}/train_mask.npy', train_mask)
+    np.save(f'{path}/test_mask.npy', test_mask)
+    np.save(f'{path}/activations.npy', activation)
+    np.save(f'{path}/y.npy', y)
 
 
 def main():
-    epochs = 7000
-    learning_rate = 0.001
-    emb_size = 32
-    batch_size = 1
-    # limit_batches = 1.0
+    # run multiple times for confidence interval - seeds generated using Google's random number generator
+    random_seeds = [42, 19, 76, 58, 92]
 
-    # train_data, test_data, in_concepts, out_concepts, concept_names, class_names = load_celeba()
-    # train_dl = DataLoader(train_data, batch_size, shuffle=True, pin_memory=True)
-    # test_dl = DataLoader(test_data, batch_size, shuffle=False, pin_memory=True)
-    #
-    # for d in train_dl:
-    #     x, t = d
-    #     c, y = t
-    #     print("c ", c.shape)
-    #     print("y ", y.shape)
-    #     print(c)
-    #     print(y)
-    # return
-
-    data, in_concepts, out_concepts, concept_names, class_names = load_ba_shapes()
-
-    for fold in range(5):
-        results_dir = f"./results/ba_shapes/{fold}/"
-        os.makedirs(results_dir, exist_ok=True)
-        model_path = os.path.join(results_dir, 'model.pt')
-
-        num_in_features = data["x"].shape[1]
-        model = DeepGraphConceptReasoner(num_in_features=num_in_features, in_concepts=in_concepts, out_concepts=out_concepts, emb_size=emb_size,
-                                    concept_names=concept_names, class_names=class_names,
-                                    learning_rate=learning_rate, loss_form=BCELoss(),
-                                    concept_loss_weight=1, class_loss_weight=0.1, logic=GodelTNorm(),
-                                    reasoner=True, temperature=1)
-
-        if not os.path.exists(model_path):
-            print(f'Running epochs={epochs}, batch_size={batch_size}, learning_rate={learning_rate}')
-            logger = TensorBoardLogger(save_dir=results_dir, name="lightning_logs")
-
-            training_loop(model=model, data=data, epochs=epochs, lr=learning_rate)
-            torch.save(model.state_dict(), model_path)
-
-        # model.load_state_dict(torch.load(model_path))
-
+    for fold, seed in enumerate(random_seeds):
+        print("\nSTART EXPERIMENT-----------------------------------------\n")
+        seed_everything(seed)
+        run_experiment(seed, fold)
+        print("\nEND EXPERIMENT-------------------------------------------\n")
 
 if __name__ == '__main__':
     main()
